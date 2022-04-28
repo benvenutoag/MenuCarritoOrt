861de1d49c8f1adf9ac0b27f70f92f3d83839055
schargo7 <schargo7@gmail.com>
2022-04-27 21:54:50 -0300

    Creación Usuario
    -Se crean Modelo y Controller de Usuario

---
+++ MenuCarritoOrt/Datos/BaseDatos.cs
@@ -0,0 +1,18 @@
+﻿using MenuCarritoOrt.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenuCarritoOrt.Datos
+{
+    public class BaseDatos : DbContext
+    {
+        public BaseDatos(DbContextOptions opciones) : base(opciones)
+        {
+
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ MenuCarritoOrt/Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenuCarritoOrt.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
