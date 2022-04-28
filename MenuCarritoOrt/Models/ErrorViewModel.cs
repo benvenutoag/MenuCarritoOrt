ec121a88d46479215f8d1d1f52d4cca2be5a8f72
benvenutoag <benvenutoag@gmail.com>
2022-04-27 21:12:12 -0300

    Agregar archivos de proyecto.

---
+++ MenuCarritoOrt/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MenuCarritoOrt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
