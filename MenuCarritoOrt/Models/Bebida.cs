fd7b0b6b91bb5078c783bfc9a16d830293355e54
benvenutoag <benvenutoag@gmail.com>
2022-05-04 21:25:48 -0300

    conBebidasPostreChequeo

---
+++ MenuCarritoOrt/Models/Bebida.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenuCarritoOrt.Models
+{
+    public class Bebida
+    {
+        public string Nombre { get; set; }
+
+        public double Precio { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public int id { get; set; }
+
+    }
+}
