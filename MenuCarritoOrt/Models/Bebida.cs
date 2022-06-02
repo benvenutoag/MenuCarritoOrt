0299075f6a5c81b692e780707c129474e9f74c04
schargo7 <schargo7@gmail.com>
2022-06-01 21:41:35 -0300

    Cambios
    Se agrega el CarritoController y View
    Se saca Producto
    Se usa el NotMapped para pasar listas

--- MenuCarritoOrt/Models/Bebida.cs
+++ MenuCarritoOrt/Models/Bebida.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MenuCarritoOrt.Models
 {
-    public class Bebida : Producto
+    public class Bebida
     {
-    
+        public string Nombre { get; set; }
 
+        public double Precio { get; set; }
+
+        public string Descripcion { get; set; }
+
+        [Key]
+        public int Id { get; set; }
     }
 }
--- MenuCarritoOrt/Models/Postre.cs
+++ MenuCarritoOrt/Models/Postre.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MenuCarritoOrt.Models
 {
-    public class Postre : Producto
+    public class Postre
     {
+        public string Nombre { get; set; }
 
+        public double Precio { get; set; }
 
+        public string Descripcion { get; set; }
 
-
+        [Key]
+        public int Id { get; set; }
     }
 }
